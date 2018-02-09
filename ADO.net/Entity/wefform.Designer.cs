1176097d7eed6508cf9144775b6bf97137d6b494
Aditya Roy Choudhary <arclive202@gmail.com>
2018-02-09 16:39:32 +0530

    Add files via upload

---
+++ ADO.net/Entity/wefform.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AdoBeginner
+{
+    partial class wefform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtCustID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCustName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMobileNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnRetrieve = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(234, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(332, 202);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtCustID
+            // 
+            this.txtCustID.Location = new System.Drawing.Point(15, 52);
+            this.txtCustID.Name = "txtCustID";
+            this.txtCustID.Size = new System.Drawing.Size(100, 20);
+            this.txtCustID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customer ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Customer Name";
+            // 
+            // txtCustName
+            // 
+            this.txtCustName.Location = new System.Drawing.Point(15, 109);
+            this.txtCustName.Name = "txtCustName";
+            this.txtCustName.Size = new System.Drawing.Size(100, 20);
+            this.txtCustName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mobile No";
+            // 
+            // txtMobileNo
+            // 
+            this.txtMobileNo.Location = new System.Drawing.Point(15, 160);
+            this.txtMobileNo.Name = "txtMobileNo";
+            this.txtMobileNo.Size = new System.Drawing.Size(100, 20);
+            this.txtMobileNo.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email ID";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(15, 215);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // btnRetrieve
+            // 
+            this.btnRetrieve.Location = new System.Drawing.Point(40, 262);
+            this.btnRetrieve.Name = "btnRetrieve";
+            this.btnRetrieve.Size = new System.Drawing.Size(75, 23);
+            this.btnRetrieve.TabIndex = 9;
+            this.btnRetrieve.Text = "Retrieve";
+            this.btnRetrieve.UseVisualStyleBackColor = true;
+            this.btnRetrieve.Click += new System.EventHandler(this.btnRetrieve_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(140, 262);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 10;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(252, 262);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(360, 262);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // wefform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnRetrieve);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMobileNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCustName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCustID);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "wefform";
+            this.Text = "wefform";
+            this.Load += new System.EventHandler(this.wefform_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtCustID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCustName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMobileNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnRetrieve;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ ADO.net/Entity/wefform.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoBeginner
+{
+    public partial class wefform : Form
+    {
+        public wefform()
+        {
+            InitializeComponent();
+        }
+
+        public void BindGrid()
+        {
+            CGIEntities obj = new CGIEntities();
+
+            var query = from customer in obj.CustomerDetails
+                        select new {
+                CustomerID = customer.CustomerID,
+                CustomerName = customer.CustomerName,
+                CustomerPhone = customer.MobileNo,
+                CustomerEmail = customer.EmailID
+                
+            };
+            dataGridView1.DataSource = query.ToList();
+        }
+        private void wefform_Load(object sender, EventArgs e)
+        {
+
+            this.BindGrid();
+        }
+
+        private void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            CGIEntities obj = new CGIEntities();
+            var query = (from customer in obj.CustomerDetails where customer.CustomerID == txtCustID.Text select customer).SingleOrDefault();
+            if(query != null)
+            {
+                txtCustID.Text = query.CustomerID;
+                txtCustName.Text = query.CustomerName;
+                txtEmail.Text = query.EmailID;
+                txtMobileNo.Text = query.MobileNo;
+                
+            }
+            else
+            {
+                MessageBox.Show("Record Not Found.");
+
+            }
+            this.BindGrid();
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            CGIEntities obj = new CGIEntities();
+            CustomerDetail cd = new CustomerDetail();
+            cd.CustomerID = txtCustID.Text;
+            cd.EmailID = txtEmail.Text;
+            cd.CustomerName = txtCustName.Text;
+            cd.MobileNo = txtMobileNo.Text;
+            obj.CustomerDetails.Add(cd);
+            int res = obj.SaveChanges();
+            if(res > 0)
+            {
+                MessageBox.Show("Success.");
+                this.BindGrid();
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            CGIEntities obj = new CGIEntities();
+            CustomerDetail cd = new CustomerDetail();
+            cd = obj.CustomerDetails.Find(txtCustID.Text);
+            cd.EmailID = txtEmail.Text;
+            cd.MobileNo = txtMobileNo.Text;
+            cd.CustomerName = txtCustName.Text;
+
+            int res = obj.SaveChanges(); //update database
+            if(res > 0)
+            {
+                MessageBox.Show("Success.");
+                this.BindGrid();
+            }
+          }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            CGIEntities obj = new CGIEntities();
+            CustomerDetail cd = new CustomerDetail();
+            cd = obj.CustomerDetails.Find(txtCustID.Text);
+            obj.CustomerDetails.Remove(cd);
+            int res = obj.SaveChanges();
+            if(res > 0)
+            {
+                MessageBox.Show("Record Deleted.");
+                this.BindGrid();
+            }
+        }
+    }
+}
